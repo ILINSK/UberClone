c18b8e634918a7ecd2420aa501ca5fff013a78a3
alexa <alexanderilinskiy2002@gmail.com>
2023-04-14 03:39:30 +0300

    Add User info and sign out

--- app/src/main/java/com/example/uberclone/Common.kt
+++ app/src/main/java/com/example/uberclone/Common.kt
@@ -6,4 +6,12 @@ object Common {
 
     var currentUser: DriverInfoModel? = null
     val DRIVER_INFO_REFERENCE: String = "DriverInfo"
+
+    fun buildWelcomeMessage(): String {
+        return StringBuilder("Добро пожаловать, ")
+            .append(currentUser!!.firstName)
+            .append(" ")
+            .append(currentUser!!.lastName)
+            .toString()
+    }
 }
